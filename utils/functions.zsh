c6d748bd50703942b5b575ef48f57d18c3057c38
luizeboli <unifelicio@gmail.com>
2020-05-29 12:12:13 -0300

    chore: update package version

--- utils/functions.zsh
+++ utils/functions.zsh
@@ -65,5 +65,5 @@ hypa::welcome() {
 /_//_/ \_, // .__/\_,_/  \___//_/ \__/ 
       /___//_/                                                                                                 
 '
-  hypa::print "${yellow}Version: 0.1.0-alpha\n"
+  hypa::print "${yellow}Version: 0.1.2-alpha\n"
 }
